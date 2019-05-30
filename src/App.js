ba042964a92449b7e3069d802477da4cf975378d
AlenaNik <alena.nik.digital@gmail.com>
2019-05-30 16:34:21 +0200

    router intalled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./layout/navbar";
+// import { Router, Route, Switch } from "react-router";
 
 function App() {
   return (
     <div>
       <NavBar />
+        {/*<BrowserRouter>*/}
+        {/*    <div>*/}
+        {/*        <NavBar />*/}
+        {/*        <Switch>*/}
+        {/*            <Route exact path="/" component={Home} />*/}
+        {/*            <Route path="#" component={Tienes una veterinaria} />*/}
+        {/*            <Route path="/timer" component={contacto}/>*/}
+        {/*            <Route path="/myapi" component={iniciar seccion }/>*/}
+        {/*        </Switch>*/}
+        {/*    </div>*/}
+        {/*</BrowserRouter>*/}
+
     </div>
   );
 }
---
+++ src/veterinarias.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import "./layout/navbar.js";
+import NavBar from "./layout/navbar";
+
+class Veterinarias extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+            </div>
+        );
+    }
+}
+
+export default Veterinarias;
